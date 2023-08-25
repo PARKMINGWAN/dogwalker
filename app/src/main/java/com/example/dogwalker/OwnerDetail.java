9cfd6716483a50e4b6eacb1cb11fffe1cf02cf90
jungyerang <132261862+jungyerang@users.noreply.github.com>
2023-08-25 16:19:12 +0900

    2023-08-25 16:10 OwnerMyPageFragment에 btnUpdate 클릭 시 OwnerDetail로 이동

---
+++ app/src/main/java/com/example/dogwalker/OwnerDetail.java
@@ -0,0 +1,28 @@
+package com.example.dogwalker;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class OwnerDetail extends AppCompatActivity {
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_owner);
+
+
+        progressBar = findViewById(R.id.progress_bar);
+
+        progressBar.setVisibility(View.INVISIBLE);
+
+    }
+
+
+}

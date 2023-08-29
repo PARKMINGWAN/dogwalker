41ee3f29dde16c71b80b151ae45fdac09c0fe6f6
PARKMINGWAN <61464552+PARKMINGWAN@users.noreply.github.com>
2023-08-29 11:35:38 +0900

    2023-08-29 11:18 박민관 예약작업 진행중 리스트까지 진행

--- app/src/main/java/com/example/dogwalker/WalkerDetail.java
+++ app/src/main/java/com/example/dogwalker/WalkerDetail.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class WalkerDetail extends AppCompatActivity {
     private ProgressBar progressBar;
@@ -28,6 +29,7 @@ public class WalkerDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_walke);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         Intent intent = getIntent();
         walkerUUID = intent.getStringExtra("walkerUUID");
         ownerUid = intent.getStringExtra("onwerUid");
@@ -38,11 +40,39 @@ public class WalkerDetail extends AppCompatActivity {
         txtWalkerCareer =findViewById(R.id.txtCareer);
         txtWalkerNurtuer=findViewById(R.id.txtNurture);
         txtWalkerTel=findViewById(R.id.txtTel);
-
+        applicationWalkerProfile= new ApplicationWalkerProfile();
         btnApplication =findViewById(R.id.btnApproval);
         btnCancel=findViewById(R.id.btnCancle);
+        Log.d("워커 디테일 오너 uid : ",ownerUid+"");
+        Log.d("워커 디테일 워커 uuid : ",walkerUUID+"");
+
+        readWalkerProfileFirebaseValue(new FirebaseCallback() {
+            @Override
+            public void onResponse(ApplicationWalkerProfile value) {
+if (value!=null)
+{
+
+    applicationWalkerProfile =value;
+    Log.d("워커디테일 객체 데이터 로그 : ",applicationWalkerProfile.getWalkerAddr()+"");
+    txtWalkerName.setText(value.getWalkerName());
+    txtWalkerAddr.setText(value.getWalkerAddr());
+    txtWalkerTel.setText(value.getWalkerTel());
+    txtWalkerNurtuer.setText(value.getWalkerNurture());
+    txtWalkerCareer.setText(value.getWalkerCareer());
 
 
+}
+            }
+        });
+
+        
+        btnApplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                applicationWalkerProfile.setIsReservation("1");//예약진행중
+                mDatabase.child("ApplicationList").child(ownerUid).child(walkerUUID).setValue(applicationWalkerProfile);
+            }
+        });
 
     }
 
@@ -50,16 +80,15 @@ public class WalkerDetail extends AppCompatActivity {
         void onResponse(ApplicationWalkerProfile value);
     }
 
-    public void readFirebaseValue(FirebaseCallback callback) {
+    public void readWalkerProfileFirebaseValue(FirebaseCallback callback) {
 
-        DatabaseReference uidRef = mDatabase.child("ApplicationList").child("owner").child(ownerUid).child(walkerUUID);
+        DatabaseReference uidRef = mDatabase.child("ApplicationList").child(ownerUid).child(walkerUUID);
         uidRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
                 if (task.isSuccessful()) {
 
                     ApplicationWalkerProfile value = task.getResult().getValue(ApplicationWalkerProfile.class);
-                    Log.d("워커 디테일 : ",value.getUid()+"");
                     callback.onResponse(value);
                 } else {
 
